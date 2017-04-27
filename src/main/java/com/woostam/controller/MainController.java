a5ceec9395aa3a2befe8f6a252585c7654fa83a3
woo <swstamwoo@gmail.com>
2017-04-27 18:47:49 +0800

    Initial commit

---
+++ src/main/java/com/woostam/controller/MainController.java
@@ -0,0 +1,17 @@
+package com.woostam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by woo on 4/27.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
